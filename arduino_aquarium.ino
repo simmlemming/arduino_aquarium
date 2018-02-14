cd72e94713c880defb554e7404f914c9e7f685f1
Maxim Tkachenko <maxim@2dehands.be>
2018-02-14 10:59:02 +0100

    Controller works with OLED display (and only one temp. sensor).

--- arduino_aquarium.ino
+++ arduino_aquarium.ino
@@ -2,56 +2,25 @@
 #include <OneWire.h>
 #include <TM74HC595Display.h>
 #include <TimerOne.h>
+#define OLED_RESET 4
 
-int SCLK = 7;
-int RCLK = 6;
-int DIO = 5;
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display(OLED_RESET);
+
 int DS18x20 = 13;
-
-TM74HC595Display first_disp(SCLK, RCLK, DIO);
-TM74HC595Display second_disp(10, 9, 8);
-
 OneWire  ds(DS18x20);  // on pin 10 (a 4.7K resistor is necessary)
 
 byte first_sensor[8] = {0x28, 0xFF, 0xA8, 0x3F, 0x80, 0x16, 0x5, 0x8C};
-byte second_sensor[8] = {0x28, 0xFF, 0x99, 0x67, 0x80, 0x16, 0x5, 0x1};
-float celsius;  
+//byte second_sensor[8] = {0x28, 0xFF, 0x99, 0x67, 0x80, 0x16, 0x5, 0x1};
+float old_value;  
 
-byte FP[10];
-byte N[10];
-
-volatile byte N1[4];
-volatile byte N2[4];
 
 void setup(void) {
-N[0] = 0xC0; //0
-N[1] = 0xF9; //1
-N[2] = 0xA4; //2
-N[3] = 0xB0; //3
-N[4] = 0x99; //4
-N[5] = 0x92; //5
-N[6] = 0x82; //6
-N[7] = 0xF8; //7
-N[8] = 0x80; //8
-N[9] = 0x90; //9
-FP[0] = 0b01000000; //.0
-FP[1] = 0b01111001; //.1
-FP[2] = 0b00100100; //.2
-FP[3] = 0b00110000; //.3
-FP[4] = 0b00011001; //.4
-FP[5] = 0b00010010; //.5
-FP[6] = 0b00000010; //.6
-FP[7] = 0b01111000; //.7
-FP[8] = 0b00000000; //.8
-FP[9] = 0b00010000; //.9
-
 //  Serial.begin(9600);
-
-  Timer1.initialize(1999);
-  Timer1.attachInterrupt(update_first_display);
-
-  MsTimer2::set(1, update_second_display);
-  MsTimer2::start();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.clearDisplay();
 }
 
 float getTemperature(byte addr[]) {
@@ -86,40 +55,49 @@ float getTemperature(byte addr[]) {
 }
 
 void loop(void) {
-  celsius = getTemperature(first_sensor);
-  fill_display_data(celsius, N1);
+  float new_value = getTemperature(first_sensor);
 //  Serial.print("t1 = ");
-//  Serial.print(celsius);
+//  Serial.println(new_value);
 
-  celsius = getTemperature(second_sensor);
-  fill_display_data(celsius, N2);
+  if (new_value != old_value) {
+    old_value = new_value;
+    update_display(new_value);
+  }
+
+//  celsius = getTemperature(second_sensor);
+//  fill_display_data(celsius, N2);
 //  Serial.print(", t2 = ");
 //  Serial.println(celsius);
 
   delay(500);
 }
 
-void fill_display_data(float number, byte data[]) {
-  int whole = (int) number;
 
-  data[0] = N[whole/10];
-  data[1] = FP[whole % 10];
+void update_display(float value) {
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  
+  display.setTextSize(2);
+  String text = "180L, C";
+  display.setCursor(128 - textWidth(text, 2), 0);
+  display.println("180L, C");
+  
+//  display.fillRect(0, 0, 12, 16, WHITE);
+//  display.println("00123456789012345678901234567890");
+//
+  text = String(value);
+  display.setTextSize(4);
+  int margin = (128 - textWidth(text, 4)) / 2 + 2;
 
-  int fr = (int) ((number - whole) * 100);
-  data[2] = N[fr / 10];
-  data[3] = N[fr % 10];
+  Serial.println(margin);
+  Serial.println(textWidth(text, 4));
+  display.setCursor(margin, 24);
+  display.println(text);
+
+  display.display();
 }
 
-void update_first_display() {
-  first_disp.send(N1[0], 0b1000);
-  first_disp.send(N1[1], 0b100);
-  first_disp.send(N1[2], 0b10);
-  first_disp.send(N1[3], 0b1);
+int textWidth(String text, int size) {
+  return text.length() * 6 * size;
 }
 
-void update_second_display() {
-  second_disp.send(N2[0], 0b1000);
-  second_disp.send(N2[1], 0b100);
-  second_disp.send(N2[2], 0b10);
-  second_disp.send(N2[3], 0b1);
-}
