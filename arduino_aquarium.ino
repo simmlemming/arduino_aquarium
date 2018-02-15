3e3d4a9ff9d4a6d407aa7d1179ca2609b5cef2b4
Maxim Tkachenko <maxim@2dehands.be>
2018-02-15 21:05:34 +0100

    Controller works with 16x2 LCD display (I2C).

--- arduino_aquarium.ino
+++ arduino_aquarium.ino
@@ -1,26 +1,30 @@
-#include <MsTimer2.h>
 #include <OneWire.h>
-#include <TM74HC595Display.h>
-#include <TimerOne.h>
-#define OLED_RESET 4
+//#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include "RTClib.h"
 
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+int DS18x20 = 12;
+OneWire  ds(DS18x20);
 
-Adafruit_SSD1306 display(OLED_RESET);
+//byte first_sensor[8] = {0x28, 0xFF, 0xA8, 0x3F, 0x80, 0x16, 0x5, 0x8C};
+byte second_sensor[8] = {0x28, 0xFF, 0x99, 0x67, 0x80, 0x16, 0x5, 0x1};
+float temperature;  
 
-int DS18x20 = 13;
-OneWire  ds(DS18x20);  // on pin 10 (a 4.7K resistor is necessary)
-
-byte first_sensor[8] = {0x28, 0xFF, 0xA8, 0x3F, 0x80, 0x16, 0x5, 0x8C};
-//byte second_sensor[8] = {0x28, 0xFF, 0x99, 0x67, 0x80, 0x16, 0x5, 0x1};
-float old_value;  
-
+LiquidCrystal_I2C lcd(0x27,16,2);  // Устанавливаем дисплей
+RTC_DS3231 rtc;
 
 void setup(void) {
-//  Serial.begin(9600);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-  display.clearDisplay();
+  Serial.begin(9600);
+
+  lcd.init();                     
+  lcd.backlight();// Включаем подсветку дисплея
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  //adjust_rtc_time();
 }
 
 float getTemperature(byte addr[]) {
@@ -55,13 +59,13 @@ float getTemperature(byte addr[]) {
 }
 
 void loop(void) {
-  float new_value = getTemperature(first_sensor);
+  float new_value = getTemperature(second_sensor);
 //  Serial.print("t1 = ");
 //  Serial.println(new_value);
 
-  if (new_value != old_value) {
-    old_value = new_value;
-    update_display(new_value);
+  if (new_value != temperature) {
+    temperature = new_value;
+    show_temperature(temperature);
   }
 
 //  celsius = getTemperature(second_sensor);
@@ -69,35 +73,40 @@ void loop(void) {
 //  Serial.print(", t2 = ");
 //  Serial.println(celsius);
 
+  show_time();
   delay(500);
 }
 
-
-void update_display(float value) {
-  display.clearDisplay();
-  display.setTextColor(WHITE);
+void show_time() {
+  DateTime now = rtc.now();
+  byte hour = now.hour();
+  byte minute = now.minute();
   
-  display.setTextSize(2);
-  String text = "180L, C";
-  display.setCursor(128 - textWidth(text, 2), 0);
-  display.println("180L, C");
+  lcd.setCursor(11, 0);
+  if (hour < 10) {
+    lcd.print(0);
+  }
   
-//  display.fillRect(0, 0, 12, 16, WHITE);
-//  display.println("00123456789012345678901234567890");
-//
-  text = String(value);
-  display.setTextSize(4);
-  int margin = (128 - textWidth(text, 4)) / 2 + 2;
+  lcd.print(hour, DEC);
+  lcd.print(":");
 
-  Serial.println(margin);
-  Serial.println(textWidth(text, 4));
-  display.setCursor(margin, 24);
-  display.println(text);
-
-  display.display();
+  if (minute < 10) {
+    lcd.print(0);
+  }
+  
+  lcd.print(minute, DEC);
 }
 
-int textWidth(String text, int size) {
-  return text.length() * 6 * size;
+void show_temperature(float value) {
+  lcd.setCursor(8, 1);
+  lcd.print(value);
+}
+
+void adjust_rtc_time() {
+    // Sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    
+    // January 21, 2014 at 3am
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
 }
 
